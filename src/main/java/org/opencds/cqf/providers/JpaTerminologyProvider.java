ca0b1462b1b4a77827b878f5a54854ecd80a1165
Chris Schuler <cschuler@myharmoniq.com>
2019-09-23 18:38:20 -0600

    Relaxing terminology expansion requirements

--- src/main/java/org/opencds/cqf/providers/JpaTerminologyProvider.java
+++ src/main/java/org/opencds/cqf/providers/JpaTerminologyProvider.java
@@ -53,7 +53,9 @@ public class JpaTerminologyProvider implements TerminologyProvider {
         ValueSet vs = null;
         if (valueSet.getId().startsWith("http://") || valueSet.getId().startsWith("https://")) {
             if (valueSet.getVersion() != null || (valueSet.getCodeSystems() != null && valueSet.getCodeSystems().size() > 0)) {
-                throw new UnsupportedOperationException(String.format("Could not expand value set %s; version and code system bindings are not supported at this time.", valueSet.getId()));
+                if (!(valueSet.getCodeSystems().size() == 1 && valueSet.getCodeSystems().get(0).getVersion() == null)) {
+                    throw new UnsupportedOperationException(String.format("Could not expand value set %s; version and code system bindings are not supported at this time.", valueSet.getId()));
+                }
             }
             IBundleProvider bundleProvider = valueSetResourceProvider.getDao().search(new SearchParameterMap().add(ValueSet.SP_URL, new UriParam(valueSet.getId())));
             List<IBaseResource> valueSets = bundleProvider.getResources(0, bundleProvider.size());
