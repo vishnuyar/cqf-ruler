efebf63d6a1ad96b65d5acfa8ae716f2763fd098
vishnu <vardhanvishnu@gmail.com>
2020-05-11 18:03:27 +0530

    added getting patients from different server

--- common/src/main/java/org/opencds/cqf/common/retrieve/JpaFhirRetrieveProvider.java
+++ common/src/main/java/org/opencds/cqf/common/retrieve/JpaFhirRetrieveProvider.java
@@ -3,9 +3,12 @@ package org.opencds.cqf.common.retrieve;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
+import org.hl7.fhir.instance.model.api.IBaseBundle;
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.opencds.cqf.common.config.HapiProperties;
 import org.opencds.cqf.cql.retrieve.*;
 import org.opencds.cqf.cql.searchparam.SearchParameterResolver;
 
@@ -13,23 +16,27 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.jpa.dao.DaoRegistry;
 import ca.uhn.fhir.jpa.dao.IFhirResourceDao;
 import ca.uhn.fhir.jpa.searchparam.SearchParameterMap;
+import ca.uhn.fhir.rest.api.CacheControlDirective;
 import ca.uhn.fhir.rest.api.server.IBundleProvider;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.util.BundleUtil;
+import ca.uhn.fhir.util.bundle.BundleEntryParts;
 
 public class JpaFhirRetrieveProvider extends SearchParamFhirRetrieveProvider {
-    
+
     DaoRegistry registry;
 
-	public JpaFhirRetrieveProvider(DaoRegistry registry, SearchParameterResolver searchParameterResolver) {
-        super (searchParameterResolver);
-		this.registry = registry;
-	}
+    public JpaFhirRetrieveProvider(DaoRegistry registry, SearchParameterResolver searchParameterResolver) {
+        super(searchParameterResolver);
+        this.registry = registry;
+    }
 
-	@Override
-	protected Iterable<Object> executeQueries(String dataType, List<SearchParameterMap> queries) {
+    @Override
+    protected Iterable<Object> executeQueries(String dataType, List<SearchParameterMap> queries) {
         if (queries == null || queries.isEmpty()) {
             return Collections.emptyList();
         }
-        
+
         List<Object> objects = new ArrayList<>();
         for (SearchParameterMap map : queries) {
             objects.addAll(executeQuery(dataType, map));
@@ -38,13 +45,12 @@ public class JpaFhirRetrieveProvider extends SearchParamFhirRetrieveProvider {
         return objects;
     }
 
-    protected Collection<Object> executeQuery(String dataType, SearchParameterMap map) {
+    protected Collection<Object> oldExecuteQuery(String dataType, SearchParameterMap map) {
         IFhirResourceDao<?> dao = this.registry.getResourceDao(dataType);
         IBundleProvider bundleProvider = dao.search(map);
-        
-        System.out.println("running Mapstring:"+dataType+map.toNormalizedQueryString(FhirContext.forR4()));
-        if (bundleProvider.size() == null)
-        {
+
+        System.out.println("running Mapstring:" + dataType + map.toNormalizedQueryString(FhirContext.forR4()));
+        if (bundleProvider.size() == null) {
             return resolveResourceList(bundleProvider.getResources(0, 10000));
         }
         if (bundleProvider.size() == 0) {
@@ -56,7 +62,7 @@ public class JpaFhirRetrieveProvider extends SearchParamFhirRetrieveProvider {
 
     public synchronized Collection<Object> resolveResourceList(List<IBaseResource> resourceList) {
         List<Object> ret = new ArrayList<>();
-        System.out.println("size is: "+resourceList.size());
+        System.out.println("size is: " + resourceList.size());
         for (IBaseResource res : resourceList) {
             Class<?> clazz = res.getClass();
             ret.add(clazz.cast(res));
@@ -64,4 +70,48 @@ public class JpaFhirRetrieveProvider extends SearchParamFhirRetrieveProvider {
         // ret.addAll(resourceList);
         return ret;
     }
+
+    protected Collection<Object> executeQuery(String dataType, SearchParameterMap map) {
+        IFhirResourceDao<?> dao = this.registry.getResourceDao(dataType);
+        FhirContext myFhirContext = FhirContext.forR4();
+        boolean local = true;
+        if (HapiProperties.getProperties().containsKey("patient_server_url")) {
+            local = false;
+        }
+
+        IGenericClient client;
+        List<IBaseResource> resourceList = new ArrayList<>();
+        if (!local) {
+            String purl = HapiProperties.getProperties().getProperty("patient_server_url");
+            client = FhirContext.forR4().newRestfulGenericClient(purl);
+
+            String searchURL = "/" + dataType + map.toNormalizedQueryString(myFhirContext);
+            System.out.println("The query string is " + client.getServerBase() + searchURL);
+            IBaseBundle bundle = client.search().byUrl(searchURL)
+                    .cacheControl(new CacheControlDirective().setNoCache(true)).execute();
+            List<BundleEntryParts> parts = BundleUtil.toListOfEntries(myFhirContext, bundle);
+            for (Iterator<BundleEntryParts> iterator = parts.iterator(); iterator.hasNext();) {
+                BundleEntryParts next = iterator.next();
+                resourceList.add(next.getResource());
+            }
+        }
+        // IGenericClient client = FhirContext.forR4()
+        // .newRestfulGenericClient("http://localhost:9999/hapi-fhir-jpaserver/fhir");
+
+        else {
+            IBundleProvider bundleProvider = dao.search(map);
+            if (bundleProvider.size() == null) {
+                return resolveResourceList(bundleProvider.getResources(0, 10000));
+            }
+            if (bundleProvider.size() == 0) {
+                System.out.println("Empty data ");
+                return new ArrayList<>();
+            }
+            resourceList = bundleProvider.getResources(0, bundleProvider.size());
+
+        }
+
+        return resolveResourceList(resourceList);
+    }
+
 }
