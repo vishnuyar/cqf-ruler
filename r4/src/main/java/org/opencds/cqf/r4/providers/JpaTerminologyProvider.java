93912dd43b9ec7019c6eeae1c5eef76cab147e7e
vishnu <vardhanvishnu@gmail.com>
2020-10-27 17:41:51 +0530

    get valueset from remote server

--- r4/src/main/java/org/opencds/cqf/r4/providers/JpaTerminologyProvider.java
+++ r4/src/main/java/org/opencds/cqf/r4/providers/JpaTerminologyProvider.java
@@ -1,28 +1,30 @@
 package org.opencds.cqf.r4.providers;
 
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.context.support.ValueSetExpansionOptions;
-import ca.uhn.fhir.jpa.rp.r4.ValueSetResourceProvider;
-import ca.uhn.fhir.jpa.searchparam.SearchParameterMap;
-import ca.uhn.fhir.jpa.term.api.ITermReadSvcR4;
-import ca.uhn.fhir.rest.api.server.IBundleProvider;
-import ca.uhn.fhir.rest.param.UriParam;
-import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
-import ca.uhn.fhir.util.FhirVersionIndependentConcept;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.CodeSystem;
 import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.ValueSet;
 import org.hl7.fhir.r4.model.ValueSet.ValueSetExpansionContainsComponent;
-import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.opencds.cqf.cql.engine.runtime.Code;
 import org.opencds.cqf.cql.engine.terminology.CodeSystemInfo;
 import org.opencds.cqf.cql.engine.terminology.TerminologyProvider;
 import org.opencds.cqf.cql.engine.terminology.ValueSetInfo;
 
-import java.util.ArrayList;
-import java.util.List;
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.support.ValueSetExpansionOptions;
+import ca.uhn.fhir.jpa.rp.r4.ValueSetResourceProvider;
+import ca.uhn.fhir.jpa.term.api.ITermReadSvcR4;
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
+import ca.uhn.fhir.util.FhirVersionIndependentConcept;
 
 public class JpaTerminologyProvider implements TerminologyProvider {
 
@@ -31,7 +33,8 @@ public class JpaTerminologyProvider implements TerminologyProvider {
     private ValueSetResourceProvider valueSetResourceProvider;
     private Bundle valueSetsBundle;
 
-    public JpaTerminologyProvider(ITermReadSvcR4 terminologySvcR4, FhirContext context, ValueSetResourceProvider valueSetResourceProvider) {
+    public JpaTerminologyProvider(ITermReadSvcR4 terminologySvcR4, FhirContext context,
+            ValueSetResourceProvider valueSetResourceProvider) {
         this.terminologySvcR4 = terminologySvcR4;
         this.context = context;
         this.valueSetsBundle = new Bundle();
@@ -41,7 +44,8 @@ public class JpaTerminologyProvider implements TerminologyProvider {
     @Override
     public synchronized boolean in(Code code, ValueSetInfo valueSet) throws ResourceNotFoundException {
         for (Code c : expand(valueSet)) {
-            if (c == null) continue;
+            if (c == null)
+                continue;
             if (c.getCode().equals(code.getCode()) && c.getSystem().equals(code.getSystem())) {
                 return true;
             }
@@ -55,10 +59,10 @@ public class JpaTerminologyProvider implements TerminologyProvider {
         boolean needsExpand = false;
         ValueSet vs = null;
         List<IBaseResource> valueSets = new ArrayList<>();
-        System.out.println("Looking for valueset: "+valueSet.getId());
-        if(valueSetsBundle.getEntry().size()>1){
+        System.out.println("Looking for valueset: " + valueSet.getId());
+        if (valueSetsBundle.getEntry().size() > 1) {
             for (Bundle.BundleEntryComponent entry : valueSetsBundle.getEntry()) {
-                if (entry.getResource().getResourceType().toString().equals("ValueSet")){
+                if (entry.getResource().getResourceType().toString().equals("ValueSet")) {
                     if (entry.getResource().getIdElement().getIdPart().equals(valueSet.getId())) {
                         valueSets.add(entry.getResource());
                     }
@@ -66,28 +70,35 @@ public class JpaTerminologyProvider implements TerminologyProvider {
                 }
             }
 
-        }
-        else if (valueSet.getId().startsWith("http://") || valueSet.getId().startsWith("https://")) {
-            if (valueSet.getVersion() != null || (valueSet.getCodeSystems() != null && valueSet.getCodeSystems().size() > 0)) {
+        } else if (valueSet.getId().startsWith("http://") || valueSet.getId().startsWith("https://")) {
+            System.out.println("looking for valueset from Remote");
+            ValueSet remoteValueSet = getRemoteValueSet(valueSet.getId());
+            valueSets.add(remoteValueSet);
+            System.out.println("verison: " + valueSet.getVersion());
+            System.out.println("codesystem size: " + valueSet.getCodeSystems().size());
+            if (valueSet.getVersion() != null
+                    || (valueSet.getCodeSystems() != null && valueSet.getCodeSystems().size() > 0)) {
                 if (!(valueSet.getCodeSystems().size() == 1 && valueSet.getCodeSystems().get(0).getVersion() == null)) {
-                    throw new UnsupportedOperationException(String.format("Could not expand value set %s; version and code system bindings are not supported at this time.", valueSet.getId()));
+                    throw new UnsupportedOperationException(String.format(
+                            "Could not expand value set %s; version and code system bindings are not supported at this time.",
+                            valueSet.getId()));
                 }
             }
-            IBundleProvider bundleProvider = valueSetResourceProvider.getDao().search(new SearchParameterMap().add(ValueSet.SP_URL, new UriParam(valueSet.getId())));
-            valueSets = bundleProvider.getResources(0, bundleProvider.size());
-            
-        }
-        else {
+            System.out.println("size of sets: " + valueSets.size());
+            // IBundleProvider bundleProvider = valueSetResourceProvider.getDao().search(new
+            // SearchParameterMap().add(ValueSet.SP_URL, new UriParam(valueSet.getId())));
+            // valueSets = bundleProvider.getResources(0, bundleProvider.size());
+
+        } else {
             vs = valueSetResourceProvider.getDao().read(new IdType(valueSet.getId()));
         }
-        
+
         if (valueSets.size() == 1) {
             vs = (ValueSet) valueSets.get(0);
-        } 
+        }
         if (valueSets.isEmpty() && vs == null) {
             throw new IllegalArgumentException(String.format("Could not resolve value set %s.", valueSet.getId()));
-        }
-        else if(valueSets.size()>1){
+        } else if (valueSets.size() > 1) {
             throw new IllegalArgumentException("Found more than 1 ValueSet with url: " + valueSet.getId());
         }
         if (vs != null) {
@@ -117,15 +128,48 @@ public class JpaTerminologyProvider implements TerminologyProvider {
 
                 return codes;
             }
-            
+
         }
 
-        List<FhirVersionIndependentConcept> expansion = terminologySvcR4.expandValueSet(new ValueSetExpansionOptions(),   valueSet.getId());
+        List<FhirVersionIndependentConcept> expansion = terminologySvcR4.expandValueSet(new ValueSetExpansionOptions(),
+                valueSet.getId());
         for (FhirVersionIndependentConcept concept : expansion) {
             codes.add(new Code().withCode(concept.getCode()).withSystem(concept.getSystem()));
         }
 
         return codes;
+    }
+
+    private ValueSet getRemoteValueSet(String valueSetUrl) {
+        try {
+            System.out.println("the baseurl is " + valueSetUrl);
+            // Adding the format parameter to get in json format
+            URL url = new URL(valueSetUrl + "?_format=json");
+            StringBuilder sb = new StringBuilder();
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            if (conn.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                String line = null;
+                while ((line = in.readLine()) != null) {
+                    sb.append(line);
+                }
+                String result = sb.toString();
+                System.out.println(result);
+                IParser parser = FhirContext.forR4().newJsonParser();
+                ValueSet vs = parser.parseResource(ValueSet.class, result);
+                System.out.println(FhirContext.forR4().newJsonParser().encodeResourceToString(vs));
+                return vs;
+            } else {
+                System.out.println(conn.getResponseCode());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+        return null;
     }
 
     @Override
